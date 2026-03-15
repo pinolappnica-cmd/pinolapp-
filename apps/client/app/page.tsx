752c141f83f92209095ced3f18adb9a274e53755
pinolappnica-cmd <pinolappnica@gmail.com>
2026-03-14 21:38:03 -0600

    Update page.tsx

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,1 +1,12 @@
+import RestaurantList from "../components/RestaurantList";
+import { prisma } from "../../../database/prisma";
 
+export default async function HomePage() {
+  const restaurants = await prisma.restaurant.findMany();
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold mb-4">Restaurants</h1>
+      <RestaurantList restaurants={restaurants} />
+    </div>
+  );
+}
